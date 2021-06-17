1ed30d46e3a856f02eb3c6247f9fc88e7accea0f
Scott Stevens <scott.stevens404@yahoo.com>
2021-06-17 15:24:47 +0100

    HP-27 added front end update functionality for the players table

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 
 // const output = document.getElementById("output");
 
@@ -9,49 +9,53 @@
 // }
 
 //read
-const output = document.getElementById("output");
+//const output = document.getElementById("output");
 
 const getPlayers = async () => {
+    const tBody = document.querySelector("tbody");
+    tBody.innerHTML = "";
     const res = await axios.get("/players/");
-    output.innerHTML = "";
-    res.data.forEach(player => renderPlayers(player));
+    // output.innerHTML = "";
+    res.data.forEach(player => {
+        renderPlayers(player)
+    });
 }
 
-const renderPlayers = ({ player_id, playerIGN, role }) => {
-    const column = document.createElement("div");
-    column.className = "col";
+const renderPlayers = player => {
+    const tBody = document.querySelector("tbody");
+    const tRow = document.createElement("tr");
 
-    const card = document.createElement("div");
-    card.className = "card";
-    column.appendChild(card);
+    tRow.appendChild(generateCell(player.playerId));
+    tRow.appendChild(generateCell(player.playerIGN));
+    tRow.appendChild(generateCell(player.role));
+    tRow.appendChild(update(player));
 
-    const cardBody = document.createElement("div");
-    cardBody.className = "card-body";
-    card.appendChild(cardBody);
+    tBody.appendChild(tRow);
+}
 
-    const playerIGNText = document.createElement("p");
-    playerIGNText.className = "card-text";
-    playerIGNText.innerText = `playerIGN: ${playerIGN}`;
-    cardBody.appendChild(playerIGNText);
+const generateCell = (data) => {
+    const tCell = document.createElement("td");
+    tCell.innerText = data;
+    tCell.className = "align-middle";
+    return tCell;
 
-    const roleText = document.createElement("p");
-    roleText.className = "card-text";
-    roleText.innerText = `role: ${role}`;
-    cardBody.appendChild(roleText);
+}
 
-    const cardFooter = document.createElement("div");
-    cardFooter.className = "card-footer";
-    card.appendChild(cardFooter);
-
-    const deleteButton = document.createElement("a");
-    deleteButton.innerText = "Delete";
-    deleteButton.className = "card-link";
-    deleteButton.addEventListener("click", function () {
-        deletePlayers(player_id);
+const update = (player) => {
+    const tCell = document.createElement("td");
+    const updatePlayer = document.createElement("button");
+    updatePlayer.innerText = "update";
+    updatePlayer.className = "btn btn-primary";
+    updatePlayer.setAttribute("type", "button");
+    updatePlayer.setAttribute("data-bs-toggle", "modal");
+    updatePlayer.setAttribute("data-bs-target", "#uPlayerModal")
+    updatePlayer.addEventListener("click", function (event) {
+        document.getElementById("updatePlayerIGN").value = player.playerIGN;
+        document.getElementById("updateRole").value = player.role;
+        document.getElementById("updatePlayerButton").setAttribute("playerID", player.playerId);
     });
-    cardFooter.appendChild(deleteButton);
-
-    output.appendChild(column);
+    tCell.appendChild(updatePlayer);
+    return tCell;
 }
 
 getPlayers();
@@ -60,6 +64,7 @@ document.getElementById("createTeam").addEventListener("submit", function (event
     event.preventDefault();
 
     const data = {
+
         playerIGN: this.playerIGN.value,
         role: this.role.value,
 
@@ -75,7 +80,26 @@ document.getElementById("createTeam").addEventListener("submit", function (event
     console.log(this);
 });
 
+document.getElementById("updateTeam").addEventListener("submit", function (event) {
+    event.preventDefault();
 
+    const playerID = document.getElementById("updatePlayerButton").getAttribute("playerID")
+    const data = {
+
+        playerIGN: this.updatePlayerIGN.value,
+        role: this.updateRole.value
+
+    }
+
+    axios.put(`/players/update/${playerID}`, data)
+        .then(res => {
+            getPlayers();
+            this.reset();
+            this.make.focus();
+        }).catch(err => console.log(err));
+
+    console.log(this);
+});
 
 const deletePlayers = async (id) => {
     const res = await axios.delete(`/players/remove/${player_id}`);
